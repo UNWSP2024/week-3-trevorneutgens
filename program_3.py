716541966877b1a97e8b27c67b778c3017e19e59
trevorneutgens <tjneutgens@students.unwsp.edu>
2024-09-20 18:00:51 -0500

    final

--- program_3.py
+++ program_3.py
@@ -12,9 +12,14 @@
 def weight_conversion(weight):
     # Calculate the shipping charge.
     shippingCost = 0.0
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
+    if weight <= 2:
+        shippingCost = 1.50 * weight
+    elif weight <= 6:
+        shippingCost = 3.00 * weight
+    elif weight <= 10:
+        shippingCost = 4.00 * weight
+    else:
+        shippingCost = 4.75 * weight
     
     return shippingCost
 
